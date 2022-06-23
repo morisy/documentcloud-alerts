7cdd76102e2980653d0037b11710c3f9a65290bd
Michael Morisy <m@morisy.com>
2022-06-23 11:27:14 -0600

    Update main.py

--- main.py
+++ main.py
@@ -9,20 +9,18 @@ from documentcloud.toolbox import grouper, requests_retry_session
 
 class Alert(AddOn):
     def main(self):
-        documents = self.client.documents.list(id__in=self.documents)
-        self.set_message("The alert has started and gotten a list of documents.")
-        self.set_message(str(type(documents)))
-        if documents:
-            documentCount = documents.len()
-            message = f"{documentCount} documents found matching your alert\n"
-            self.set_message(message)
-            for d in documents:
-                message += f"{d.title} - {d.canonical_url}\n"
-            self.send_mail("New documents found!", message)
-            if self.data.get("slack_webhook"):
-                SLACK_WEBHOOK = self.data.get("slack_webhook")
-                requests_retry_session().post(
-                SLACK_WEBHOOK, json={"text": message}
+        if not self.documents:
+            self.set_message("No documents matching query found.")
+            return
+        message = "Documents found matching your alert\n"
+        self.set_message(message)
+        for document in self.client.documents.list(id__in=self.documents):
+            message += f"{d.title} - {d.canonical_url}\n"
+        self.send_mail("New documents found!", message)
+        if self.data.get("slack_webhook"):
+            SLACK_WEBHOOK = self.data.get("slack_webhook")
+            requests_retry_session().post(
+            SLACK_WEBHOOK, json={"text": message}
             )
 
 
